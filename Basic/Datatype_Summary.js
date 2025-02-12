f701c6396a623293e4b9b75f3f86401a7bfbcb9e
Amjad Ali <152184923+Am258031@users.noreply.github.com>
2025-02-12 06:21:59 +0000

    done

--- Basic/Datatype_Summary.js
+++ Basic/Datatype_Summary.js
@@ -11,7 +11,7 @@ const outsideTemp = null
 let userEmail;
 
 const id = Symbol('121')
-const  anotherId = Symbol('123')
+const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
@@ -27,8 +27,32 @@ let myObj = {
     age: 22
 }
 
-const myFunction = function(){
+const myFunction = function () {
     console.log("Hello World");
 }
 
 console.log(typeof myFunction);
+
+// *************************************** */
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myName = "Amjad"
+
+let anotherName = myName
+anotherName = "Ali"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "Amjad5342@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
