ff6d8a04415725a364f453fad8584499378eabe6
Amjad Ali <152184923+Am258031@users.noreply.github.com>
2025-02-12 05:35:27 +0000

    done

--- Basic/conversionOperation.js
+++ Basic/conversionOperation.js
@@ -4,7 +4,7 @@ such as type coercion(automatic) or type casting (explicit) */
 let score = "33"
 
 console.log(typeof score);
-console.log(typeof(score));
+console.log(typeof (score));
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
@@ -28,3 +28,37 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ******************** Operations ************************** //
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = "Amjad"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+// Link to study more : 
+// https://262.ecma-international.org/
